14b28aea566eabe071b2c90775cd84ef79ef147e
Your Name <you@example.com>
2025-02-04 20:04:27 +0700

    Xóa các file workflow cũ và thêm thư mục Case_Team_3

---
+++ Case_Team_3/src/main/java/com/example/case_team_3/model/Booking.java
@@ -0,0 +1,30 @@
+package com.example.case_team_3.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "booking")
+public class Booking {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime bookingCheckInDate;
+    private LocalDateTime bookingCheckOutDate;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus bookingStatus;
+    private LocalDateTime bookingUpdateTime;
+    public enum BookingStatus {
+        confirmed, completed, canceled
+    }
+}
---
+++ Case_Team_3/src/main/java/com/example/case_team_3/model/Message.java
@@ -0,0 +1,19 @@
+package com.example.case_team_3.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "messages")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sender;
+    private String receiver;
+    private String content;
+    private LocalDateTime timestamp;
+}

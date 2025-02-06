edc5b46456dcf204163354956ca82f8b6b3b6037
HauCG <maitrunghau9817189@gmail.com>
2025-02-06 23:06:02 +0700

    hello_world

---
+++ src/main/java/com/example/webbookinghomestay/WebBookingHomeStayApplication.java
@@ -0,0 +1,13 @@
+package com.example.webbookinghomestay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBookingHomeStayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBookingHomeStayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webbookinghomestay/WebBookingHomeStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webbookinghomestay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBookingHomeStayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
